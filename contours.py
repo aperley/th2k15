544146aff9644201ac3051528d4d4b415e54b25c
AllisonFisher <allisonf@andrew.cmu.edu>
2015-02-07 02:45:24 -0500

    Nighty night!

--- contours.py
+++ contours.py
@@ -17,9 +17,9 @@ import cv2.cv as cv
 from crop import crop
 
 
-im = cv2.imread('test3.png', cv2.CV_LOAD_IMAGE_GRAYSCALE)
-disp = crop(im)
+im = cv2.imread('test.png', cv2.CV_LOAD_IMAGE_GRAYSCALE)
 
+disp = crop(im)
 
 
 def getBounds(img):
@@ -31,9 +31,6 @@ def getBounds(img):
                                     cv2.THRESH_BINARY_INV, 11, 3)
 
 
-
-   
-    
     kernel = np.ones((5, 5), np.uint8)
     thresh = cv2.dilate(thresh, kernel, iterations=2)
     thresh = cv2.erode(thresh, kernel, iterations=2)
@@ -46,22 +43,12 @@ def getBounds(img):
     contours, hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,
                                            cv2.CHAIN_APPROX_SIMPLE)
 
-    print contours
+    #print contours
     contours = filter(lambda c: len(c) > 50, contours)
 
-    cv2.drawContours(thresh_copy, contours, -1, (0,255,0), 3)
+    blank_thresh = np.copy(thresh_copy)
 
-
-    def on_mouse(event, x, y, flags, thing):
-        if event == cv.CV_EVENT_LBUTTONDOWN:
-            print (x, y)
-            for i, contour in enumerate(contours):
-                dist = cv2.pointPolygonTest(contour, (x, y), False)
-                if dist > 0:
-                    print "We're inside contour %d" % i
-
-
-    
+                            
     cv2.namedWindow('image')
     cv.SetMouseCallback('image', on_mouse, 0)
     cv2.imshow('image',thresh_copy)
@@ -69,6 +56,31 @@ def getBounds(img):
     cv2.waitKey(0) & 0xFF
     cv2.destroyAllWindows()
     
+    return (contours, thresh_copy)
+
+def onContact(contours,contourIdx,thresh_copy):
+    cnt = contours[contourIdx]
+    color = (0,255,0)
+    # Create an anonymous image to display till release
+    t = np.copy(thresh_copy)
+    cv2.drawContours(t,[cnt],-1,color,-1)
+    cv2.imshow('image',t)
+
+def onRelease(contourIdx,blank_thresh):
+    cv2.imshow('image',blank_thresh)
+
+def on_mouse(event, x, y, flags, thing):
+    global mode
+    if event == cv.CV_EVENT_LBUTTONDOWN:
+        print (x, y)
+        for i, contour in enumerate(contours):
+            dist = cv2.pointPolygonTest(contour, (x, y), False)
+            if dist > 0:
+                    onContact(i)
+                    return
+        onRelease(0)
+
 
 
 getBounds(disp)
+
