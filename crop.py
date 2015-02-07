cb648a8bd4895ea79b9f0d73a61bfea2c4c456c6
Aaron Perley <aaron.perley@gmail.com>
2015-02-06 20:50:18 -0500

    Add perspective cropping module

---
+++ crop.py
@@ -0,0 +1,66 @@
+import cv2
+import cv2.cv as cv
+import numpy as np
+
+def order_points(pts):
+    # top-left, top-right, bottom-right, bottom-left
+    rect = np.zeros((4, 2), dtype=np.float32)
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+
+    return rect
+
+def four_point_transform(image, pts):
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[0] - bl[0]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[0] - tl[0]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+ 
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[1] - br[1]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[1] - bl[1]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+    
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype = "float32")
+ 
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+ 
+    # return the warped image
+    return warped
+
+def crop(im):
+    im = cv2.medianBlur(im, 5)
+    ret, thresh = cv2.threshold(im, 110, 255,
+                                cv2.THRESH_BINARY_INV)
+    cim = cv2.cvtColor(im, cv2.COLOR_GRAY2BGR)
+
+
+    circles = cv2.HoughCircles(im, cv.CV_HOUGH_GRADIENT, 1, 20,
+                               param1=50, param2=30,
+                               minRadius=0, maxRadius=0)
+
+    pts = []
+    for cir in circles[0]:
+        #cv2.circle(cim, (cir[0], cir[1]), cir[2], (0, 0, 255), 1)
+        pts.append(cir[0:2])
+    pts = np.array(pts)
+
+    warped = four_point_transform(cim, pts)
+    return warped
+
+
